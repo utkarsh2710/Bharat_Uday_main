3ab4ad0434b132acd193327b0730c6d47e27906b
Utkarsh Pandey <utkarsh.pandey2710@gmail.com>
2021-09-15 12:48:39 +0530

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-
-    </>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
---
+++ frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>Bharat Uday</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='link2'>Link2</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='link3'>link3</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='link4'>link4</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
