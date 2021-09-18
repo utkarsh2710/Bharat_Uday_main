7134a7917aab9fef0aa2013c64713ee7cafa110a
Utkarsh Pandey <utkarsh.pandey2710@gmail.com>
2021-09-18 13:30:54 +0530

    sign in, sign up page added

--- frontend/src/pages/signin.js
+++ frontend/src/pages/signin.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import SignIn from '../components/signin/SignIn'
+import SignUp from '../components/signup/SignUp'
 
-const signin = () => {
+const Signin = () => {
+    const [value, setValue] = useState(0)
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const paperStyle = { width: 340, margin: "20px auto" }
+    function TabPanel(props) {
+        const { children, value, index, ...other } = props;
+
+        return (
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`simple-tabpanel-${index}`}
+                aria-labelledby={`simple-tab-${index}`}
+                {...other}
+            >
+                {value === index && (
+                    <Box>
+                        <Typography>{children}</Typography>
+                    </Box>
+                )}
+            </div>
+        );
+    }
+
     return (
-        <div>
-            <h2>Sign in</h2>
-        </div>
+        <Paper elevation={20} style={paperStyle}>
+            <Tabs
+                value={value}
+                indicatorColor="primary"
+                textColor="primary"
+                onChange={handleChange}
+                aria-label="disabled tabs example"
+            >
+                <Tab label="Sign In" />
+
+                <Tab label="Sign Up" />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+                <SignIn handleChange={handleChange} />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <SignUp />
+            </TabPanel>
+        </Paper>
+
     )
 }
 
-export default signin
+export default Signin
