e71955d0af85303f92944c67e4e5af55fd89fff0
manan-1 <manan@hertzai.com>
2022-06-17 18:45:08 +0530

    added about_us page

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import NotHomeButton from "./NotHomeButton";
+
+
+function App(){
+    return (
+        <div>
+        <NotHomeButton 
+        title="Foundations Learning Program" 
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor eu augue ut lectus arcu bibendum. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Enim neque volutpat ac tincidunt vitae semper quis. Gravida rutrum quisque non tellus orci ac auctor augue. Scelerisque in dictum non consectetur a erat. Eget sit amet tellus cras adipiscing enim. Commodo quis imperdiet massa tincidunt. Dictum fusce ut placerat orci nulla pellentesque dignissim."/>
+        
+        <br></br>
+        <NotHomeButton 
+        title="Life Skills Program" 
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor eu augue ut lectus arcu bibendum. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Enim neque volutpat ac tincidunt vitae semper quis. Gravida rutrum quisque non tellus orci ac auctor augue. Scelerisque in dictum non consectetur a erat. Eget sit amet tellus cras adipiscing enim. Commodo quis imperdiet massa tincidunt. Dictum fusce ut placerat orci nulla pellentesque dignissim."/>
+
+        <br></br>
+        <NotHomeButton 
+        title="Mentoring Programs" 
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor eu augue ut lectus arcu bibendum. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Enim neque volutpat ac tincidunt vitae semper quis. Gravida rutrum quisque non tellus orci ac auctor augue. Scelerisque in dictum non consectetur a erat. Eget sit amet tellus cras adipiscing enim. Commodo quis imperdiet massa tincidunt. Dictum fusce ut placerat orci nulla pellentesque dignissim."/>
+        
+        <br></br>
+        <NotHomeButton 
+        title="Foundations Learning Program" 
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor eu augue ut lectus arcu bibendum. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Enim neque volutpat ac tincidunt vitae semper quis. Gravida rutrum quisque non tellus orci ac auctor augue. Scelerisque in dictum non consectetur a erat. Eget sit amet tellus cras adipiscing enim. Commodo quis imperdiet massa tincidunt. Dictum fusce ut placerat orci nulla pellentesque dignissim."/>
+
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
---
+++ src/components/ButtonText.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ButtonText(props){
+    return <p style={{marginTop:"10px", transition:"margin-bottom 5s" }}>{props.text}</p>
+}
+
+
+export default ButtonText;
